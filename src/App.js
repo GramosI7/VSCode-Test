594ef0ee76c3079a3c1ef8247fb6567ae95af54e
GramosI7 <imeri.gramos@gmail.com>
2018-08-06 16:27:29 +0200

    2eme commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>Salut a tous</h1>
+        <h2>Tuto sur ceci</h2>
       </div>
     );
   }
